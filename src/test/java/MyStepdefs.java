85bfe400216c4be8463a1c1df0efe0763ac81bd1
tamaraalsaad <tamaraalsaad@yahoo.com>
2025-12-16 22:11:03 +0100

    uppdet

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -13,13 +13,15 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class MyStepdefs {
     private WebDriver driver;
     private WebDriverWait wait;
 
-    @Given("I open the register page in {}")
+    @Given("I open the register page in {string}")
     public void iOpenTheRegisterPageIn(String browser) {
 
         if (browser.equals("chrome")) {
@@ -29,72 +31,93 @@ public class MyStepdefs {
         }
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.get("file:///C:/Users/tamar/Downloads/Register/Register.html");
+        assertTrue(driver.findElement(By.className("page-wrapper")).isDisplayed());
     }
 
-    private WebElement waitForVisible(By locator) {
+    public WebElement waitForVisible(By locator) {
 
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
 
     }
+
+
+
 
     @When("I fill in the register page form with{string} {string} {string} {string} {string}{string} {string} {string}")
     public void iFillInTheRegisterPageFormWith(String dateOfBirth,
                                                String firstName,
                                                String lastName, String email,
                                                String confirmEmail,
-                                               String password, String confirmPassword,
-                                               String acceptTerms) {
+                                               String password, String confirmPassword) {
 
-        WebElement dobInput = waitForVisible(By.xpath("/html/body/div[1]/div[2]/div/div/div/div/div/div/div/form/div[2]/div/div/input"));
-        dobInput.clear();
-        dobInput.sendKeys(dateOfBirth);
-        dobInput.sendKeys(Keys.TAB);
-
-        driver.findElement(By.id("member_firstname")).sendKeys(firstName);
+            WebElement dobInput = waitForVisible(By.xpath("/html/body/div[1]/div[2]/div/div/div/div/div/div/div/form/div[2]/div/div/input"));
+            dobInput.clear();
+            dobInput.sendKeys(dateOfBirth);
+            dobInput.sendKeys(Keys.TAB);
+       driver.findElement(By.id("member_firstname")).sendKeys(firstName);
         driver.findElement(By.id("member_lastname")).sendKeys(lastName);
         driver.findElement(By.id("member_emailaddress")).sendKeys(email);
         driver.findElement(By.id("member_confirmemailaddress")).sendKeys(confirmEmail);
         driver.findElement(By.id("signupunlicenced_password")).sendKeys(password);
         driver.findElement(By.id("signupunlicenced_confirmpassword")).sendKeys(confirmPassword);
-        if (Boolean.parseBoolean(acceptTerms)) {
-            driver.findElement(By.xpath("//label[contains(.,'Terms and Conditions')]")).click();
-            driver.findElement(By.xpath("//label[contains(.,'aged over 18')]")).click();
-            driver.findElement(By.xpath("//label[contains(.,'Code of Ethics')]")).click();
 
-
-        }
     }
 
     @And("I accept the Account Confirmation")
     public void iAcceptTheAccountConfirmation() {
+        driver.findElement(By.xpath("//label[contains(.,'Terms and Conditions')]")).click();
+        driver.findElement(By.xpath("//label[contains(.,'aged over 18')]")).click();
+
     }
 
     @And("I red and accept to \\(applies to all Members)")
     public void iRedAndAcceptToAppliesToAllMembers() {
-    }
-
-    @And("I submit the registration form")
-    public void iSubmitTheRegistrationForm() {
+        driver.findElement(By.xpath("//label[contains(.,'Code of Ethics')]")).click();
 
     }
+
     @And("I confirm and join")
     public void iConfirmAndJoin() {
+
         driver.findElement(By.cssSelector("input[value='CONFIRM AND JOIN']")).click();
     }
 
-
     @Then("I should see a success message {string}")
-    public void iShouldSeeASuccessMessage(String A104955 ) {
+    public void iShouldSeeASuccessMessage(String A104955) {
         driver.get("file:///C:/Users/tamar/Downloads/Register/Success.html");
-       WebElement successMessage  = driver.findElement(By.cssSelector("body > div > div.page-content-wrapper > div > div > h2"));
+        WebElement successMessage = driver.findElement(By.cssSelector("body > div > div.page-content-wrapper > div > div > h2"));
 
-        assertEquals(A104955,successMessage.getText() );
+        assertEquals(A104955, successMessage.getText());
+
+
     }
+
+    @When("I fill in the register page and the last name missing")
+    public void iFillInTheRegisterPageAndTheLastNameMissing() {
+
+        WebElement lastNameField = waitForVisible(By.id("member_lastname"));
+        lastNameField.clear();
+
+        assertTrue("Efternamnsfältet borde vara tomt", lastNameField.getAttribute("value").isEmpty());
+    }
+
 
     @Then("I should see an error message {string}")
-    public void iShouldSeeAnErrorMessage(String arg0) {
+    public void iShouldSeeAnErrorMessage(String expectedMessage) {
+        assertNotNull(driver, "Driver är null i Then-steget! Har Given-steget körts?");
+
+        String actualPageText = waitForVisible(By.cssSelector(".warning field-validation-error")).getText();
+
+        assertTrue( actualPageText,actualPageText.contains(expectedMessage));
+
     }
 }
+
+
+
+
+
+
 
 
 
--- src/test/resources/Feactures/register.feature
+++ src/test/resources/Feactures/register.feature
@@ -1,29 +1,28 @@
 Feature: User registration
 
   Scenario Outline: When I fill in the registration form with
-    Given I open the register page in <browser>
+    Given I open the register page in "<browser>"
     When  I fill in the register page form with"<datOfBirth>" "<firstName>" "<lastName>" "<email>" "<confirmEmail>""<password>" "<confirmPassword>" "<acceptTerms>"
     And  I accept the Account Confirmation
     And  I red and accept to (applies to all Members)
-    And I submit the registration form
     And I confirm and join
     Then  I should see a success message "A104955"
     Examples:
       | browser | datOfBirth | firstName | lastName | email                | confirmEmail         | password | confirmPassword | acceptTerms |
-      | chrome  | 09/01/1978 | Tamra     | Alsaad   | tamara@test.se       | tamara@test.se       | 123456   | 123456          | true        |
-      | edge    | 19/05/2012 | Josef     | Ataiwi   | josef.ataiwi@test.se | josef.ataiwi@test.se | 120519   | 120519          | true        |
+      | chrome  | 09/01/1978 | Tamara    | Alsaad   | tamara@test.se       | tamara@test.se       | 123456   | 123456          | true        |
+      | edge    | 23/09/2007 | Josef     | Ataiwi   | josef.ataiwi@test.se | josef.ataiwi@test.se | 120519   | 120519          | true        |
 
   Scenario: missing_lastname
-
-    And I submit the registration form
+    Given  I open the register page in "chrome"
+    When I fill in the register page and the last name missing
     Then I should see an error message "Last name is required"
 
   Scenario:password_mismatch
 
-    And I submit the registration form
+
     Then  I should see an error message "Passwords do not match"
 
   Scenario: not_accepted
 
-    And I submit the registration form
+
     Then I should see an error message "You must accept terms and conditions"
